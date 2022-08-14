9d527f32ad44466a591a28fa3b6620ec7a34c97b
Volodymyr Husak <volodyaog2907@gmail.com>
2022-08-14 21:49:11 +0300

    adds startup files

---
+++ js/01-gallery.js
@@ -0,0 +1,7 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
+console.log(galleryEl);

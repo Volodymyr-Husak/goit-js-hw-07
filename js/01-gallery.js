cd0f629f3d657921427a625d90b65d71b372da46
Volodymyr Husak <volodyaog2907@gmail.com>
2022-08-14 22:18:10 +0300

    creates a markup of gallery elements

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,7 +1,32 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
 
 const galleryEl = document.querySelector(".gallery");
-console.log(galleryEl);
+// console.log(galleryEl);
+
+const markup = galleryItems
+  // event.preventDefault();
+  .map(
+    (galleryItem) =>
+      `<div class="gallery__item">
+            <a class="gallery__link" href='${galleryItem.original}'>
+                <img
+                    class="gallery__image"
+                    src='${galleryItem.preview}'
+                    data-source='${galleryItem.original}'
+                    alt='${galleryItem.description}'
+                />
+            </a>
+        </div>`
+  )
+  .join("");
+
+galleryEl.insertAdjacentHTML("beforeend", markup);
+
+// ===============================================================================================
+const galleryItemEl = document.querySelectorAll(".gallery__item");
+console.log(galleryItemEl);
+// galleryItemEl
+// galleryItemEl.forEach((item) => item.preventDefault());

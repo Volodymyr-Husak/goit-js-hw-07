ce1f098d397bbe74b3d57638353cbf34eed7862c
Volodymyr Husak <volodyaog2907@gmail.com>
2022-08-15 19:43:59 +0300

    creats task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
+
+const markup = galleryItems
+
+  .map(
+    (galleryItem) =>
+      `<a class="gallery__item" href="${galleryItem.original}">
+            <img class="gallery__image" src="${galleryItem.preview}" alt="${galleryItem.description}" /> />
+        </a>`
+  )
+  .join("");
+
+galleryEl.insertAdjacentHTML("beforeend", markup);
+
+// ===========================================================================
+
+new SimpleLightbox(".gallery a", {
+  /* options */
+  captionsData: "alt",
+  captionDelay: 250,
+});
